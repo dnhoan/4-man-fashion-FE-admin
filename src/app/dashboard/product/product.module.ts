c9957856fefdab3bca99ea8a996875fb24f5a266
hoanam2k2 <hoanam2k2.tb@gmail.com>
2023-03-02 22:10:33 +0700

    create product

--- src/app/dashboard/product/product.module.ts
+++ src/app/dashboard/product/product.module.ts
@@ -4,9 +4,16 @@ import { CreateProductComponent } from './create-product/create-product.componen
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'src/app/share_modules/ng-add-ng-zorro-antd.module';
 import { CommonModule } from '@angular/common';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgZorroAntdModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule,
+    QuillModule.forRoot(),
+  ],
   declarations: [ProductDetailComponent, CreateProductComponent],
 })
 export class ProductModule {}
